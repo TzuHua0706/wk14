671b0d59099a92dacc013d36e722e8992314ac76
TzuHua0706 <penny20801@gmail.com>
2016-12-20 13:48:26 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $(".habrgr").click(function(){   /*漢堡選單*/
+    if( $("nav").hasClass("click") ){
+      $("nav").removeClass("click");
+      $("nav ul").slideUp();
+    }else{
+      $("nav").addClass("click");
+      $("nav ul").slideDown();
+      return false;
+    }
+  });
+
+
+});
